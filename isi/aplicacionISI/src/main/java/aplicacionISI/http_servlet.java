9a44717a727a4fea3e84087b8ef76f2cbb1d3d9e
EugeniaCasFrag <castillafrage@gmail.com>
2019-05-11 12:08:46 +0200

    juhygt

--- isi/aplicacionISI/src/main/java/aplicacionISI/http_servlet.java
+++ isi/aplicacionISI/src/main/java/aplicacionISI/http_servlet.java
@@ -1,6 +1,12 @@
 package aplicacionISI;
 
+import java.io.IOException;
 
-public class http_servlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+public class http_servlet{
+	public void doGet (HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException{processRequest(request, response)};
 }
